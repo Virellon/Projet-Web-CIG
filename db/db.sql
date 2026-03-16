5a9f13d03ad0a5b21a3861cd825df61fb6a6192c
ibrahima soumare <soumareibrahima178@gmail.com>
2026-03-16 15:47:04 +0100

    db dieuxiem version

--- db/db.sql
+++ db/db.sql
@@ -1,8 +1,16 @@
-create database cubic_infrastructure group;
-user_cubic__infrastructure group;
-
+-- create database cubic_infrastructure group;
+-- user_cubic__infrastructure group;
 
 create table user(
+id serial primary key ,
+username varchar(100)not null unique ,
+email varchar(100) not null unique ,
+password varchar(250),
+role varchar(20) default 'player',
+created_at timestamp default current_timestamp 
+);
+
+
 
 
 
